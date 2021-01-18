2e9f4887b2aff79e3426433df34954d314e49e13
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-18 19:56:45 +0530

    add style to component

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./NavBar.css";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 function NavBar() {
   return (
@@ -21,28 +22,34 @@ function NavBar() {
         </div>
       </div>
       <div className="navBar_bottom">
+        <div className="navBar_bottomClose">
+          <IconButton aria-label="delete" className="navBar_close">
+            <CloseIcon fontSize="large" style={{ color: "black" }} />
+          </IconButton>
+        </div>
+
         <ul className="navBar_list">
-          <li>
+          <li className="navBar_li">
             <a className="navBar_link" href="#home">
               HOME
             </a>
           </li>
-          <li>
+          <li className="navBar_li">
             <a className="navBar_link" href="#about">
               ABOUT
             </a>
           </li>
-          <li>
+          <li className="navBar_li">
             <a className="navBar_link" href="#skills">
               SKILLS
             </a>
           </li>
-          <li>
+          <li className="navBar_li">
             <a className="navBar_link" href="#project">
               PROJECT
             </a>
           </li>
-          <li>
+          <li className="navBar_li">
             <a className="navBar_link" href="#contact">
               CONTACT
             </a>
