8dee306a4e41c88c8a346e21e87e97d6bc803c33
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-18 17:59:11 +0530

    made navBar_left and navBar_right

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,10 +3,8 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <div className="navBar">
-      <div className="navBar_logo">
-        <span className="navBar_logo1"></span>
-        <span className="navBar_logo1"></span>
-      </div>
+      <div className="navBar_left"></div>
+      <div className="navBar_right"></div>
     </div>
   );
 }
