9e57c1237ee4e61282c4065d64a2a79c9eadcb78
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-18 18:06:28 +0530

    add button

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./NavBar.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+
 function NavBar() {
   return (
     <div className="navBar">
@@ -8,7 +11,13 @@ function NavBar() {
           <h1>NavBar</h1>
         </div>
       </div>
-      <div className="navBar_right"></div>
+      <div className="navBar_right">
+        <div className="navBar_icon">
+          <IconButton aria-label="delete">
+            <MenuIcon fontSize="large" style={{ color: "white" }} />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
