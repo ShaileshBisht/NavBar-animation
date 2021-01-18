cb9f4ef9e819a4c83286b48407c43e8f36b5b1fe
shaileh Bisht <shaileshbisht765@gmail.com>
2021-01-18 18:12:44 +0530

    add navBar_top div

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,16 +6,18 @@ import IconButton from "@material-ui/core/IconButton";
 function NavBar() {
   return (
     <div className="navBar">
-      <div className="navBar_left">
-        <div className="navBar_logo">
-          <h1>NavBar</h1>
+      <div className="navBar_top">
+        <div className="navBar_left">
+          <div className="navBar_logo">
+            <h1>NavBar</h1>
+          </div>
         </div>
-      </div>
-      <div className="navBar_right">
-        <div className="navBar_icon">
-          <IconButton aria-label="delete">
-            <MenuIcon fontSize="large" style={{ color: "white" }} />
-          </IconButton>
+        <div className="navBar_right">
+          <div className="navBar_icon">
+            <IconButton aria-label="delete">
+              <MenuIcon fontSize="large" style={{ color: "white" }} />
+            </IconButton>
+          </div>
         </div>
       </div>
     </div>
